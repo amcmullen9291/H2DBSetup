631546dcea257e56c6a91ad65703c0e4bf586a85
amcmullen9291 <amcmullen9291@gmail.com>
2022-01-03 11:12:04 -0500

    init commit. applications.properties values working

---
+++ src/main/java/com/H2DataBase/H2DataBaseApplication.java
@@ -0,0 +1,13 @@
+package com.H2DataBase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class H2DataBaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(H2DataBaseApplication.class, args);
+	}
+
+}
